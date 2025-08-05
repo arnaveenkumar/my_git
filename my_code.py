8fdd2fd5d318f62664d5787eaeda840e3caeb5b6
Naveenkumar <arnaveen99@gmail.com>
2025-08-05 00:52:27 -0400

    Add class in my_code.py

--- my_code.py
+++ my_code.py
@@ -1,14 +1,16 @@
-def split_words(text):
+class SplitWords:
+    def split_words(self, text):
 
-    cleaned_text = ""
-    for char in text:
-        if ('A' <= char <= 'Z') or ('a' <= char <= 'z') or ('0' <= char <= '9') or char == ' ':
-            cleaned_text += char
+        cleaned_text = ""
+        for char in text:
+            if ('A' <= char <= 'Z') or ('a' <= char <= 'z') or ('0' <= char <= '9') or char == ' ':
+                cleaned_text += char
 
-    new_list = []
-    for word in cleaned_text.split():
-        new_list.append(word)
+        new_list = []
+        for word in cleaned_text.split():
+            new_list.append(word)
 
-    return new_list
-
-print(split_words("Happy birthday!!! to you @123"))
+        return new_list
+sw = SplitWords()
+result = sw.split_words("Happy birthday!!! to you @123")
+print(result)
