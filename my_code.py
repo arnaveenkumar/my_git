325858e92a23dd35623093194cc31b003e3fa3cd
Naveenkumar <arnaveen99@gmail.com>
2025-08-05 00:36:21 -0400

    Add my_code.py

--- my_code.py
+++ my_code.py
@@ -1,1 +1,14 @@
-This is my code
+def split_words(text):
+
+    cleaned_text = ""
+    for char in text:
+        if ('A' <= char <= 'Z') or ('a' <= char <= 'z') or ('0' <= char <= '9') or char == ' ':
+            cleaned_text += char
+
+    new_list = []
+    for word in cleaned_text.split():
+        new_list.append(word)
+
+    return new_list
+
+print(split_words("Happy birthday!!! to you @123"))
